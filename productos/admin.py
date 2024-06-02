0cd595a7b70fe609fe8b707b658964804575269e
Caiza Tacuri Alisson Lisbeth <alcaiza3@espe.edu.ec>
2024-06-01 23:19:21 -0500

    Eliminar

--- productos/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Producto # importamos el modelo Producto
-
-admin.site.register(Producto) # registramos el modelo Producto en el admin que no es tan necesario pero es una buena practica
